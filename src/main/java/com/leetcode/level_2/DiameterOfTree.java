404fc679794713f32bb06e984a14cbe9f6067429
Nagesh Vashist <nagesh113041521@gmail.com>
2022-12-16 15:33:30 +0530

    Find diameter of a tree

--- src/main/java/com/leetcode/level_2/DiameterOfTree.java
+++ src/main/java/com/leetcode/level_2/DiameterOfTree.java
@@ -1,4 +1,35 @@
 package com.leetcode.level_2;
 
+import trees.TreeNode;
+
 public class DiameterOfTree {
+    int max=0;
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        TreeNode r=root.right = new TreeNode(3);
+        r.left=new TreeNode(4);
+        r.right= new TreeNode(5);
+
+        System.out.println(new DiameterOfTree().diameterOfBinaryTree(root));
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        hight(root);
+        return max;
+    }
+
+    public int hight(TreeNode root){
+        if(root==null){
+            return  0;
+        }
+        int l = hight(root.left);
+
+        int r = hight(root.right);
+
+        max= Math.max(l+r,max);
+
+        return  Math.max(l,r)+1;
+    }
+
+
 }
